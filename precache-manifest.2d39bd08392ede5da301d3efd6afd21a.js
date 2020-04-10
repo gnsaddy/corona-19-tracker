0bd228fdd423582f963088e7ae8183dfcdd83bbb
Aditya Raj <aditya.x510@gmail.com>
2020-04-10 17:52:29 +0530

    Updates

---
+++ precache-manifest.2d39bd08392ede5da301d3efd6afd21a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "682a6b7cf37d7ab1d86f77f1054f610f",
+    "url": "/corona-19-tracker/index.html"
+  },
+  {
+    "revision": "93a18634ac69f72e8f97",
+    "url": "/corona-19-tracker/static/css/main.1a317d0a.chunk.css"
+  },
+  {
+    "revision": "2fa84f460f57ad040906",
+    "url": "/corona-19-tracker/static/js/2.f44e3334.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-19-tracker/static/js/2.f44e3334.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93a18634ac69f72e8f97",
+    "url": "/corona-19-tracker/static/js/main.f006a303.chunk.js"
+  },
+  {
+    "revision": "82e321f264972f606140",
+    "url": "/corona-19-tracker/static/js/runtime-main.d897afaf.js"
+  }
+]);
